3b3cc110b011fc8605a5661c6e4bd5a4698196d7
KobertRubica <stilic97@gmail.com>
2020-04-17 16:06:20 +0200

    li

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 449);
             this.Name = "Simulator";
-            this.Text = "faedf";
+            this.Text = "Simulator";
             this.ResumeLayout(false);
 
         }
