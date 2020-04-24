94a105567907358fed2d5ab6e3673a9e75367742
KobertRubica <stilic97@gmail.com>
2020-04-24 19:46:55 +0200

    symulator ma już duuużo funkcji

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonSYM = new System.Windows.Forms.Button();
-            this.labelResult = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.checkTemp = new System.Windows.Forms.CheckBox();
             this.checkCis = new System.Windows.Forms.CheckBox();
@@ -42,43 +40,21 @@
             this.checkNAPW = new System.Windows.Forms.CheckBox();
             this.checkNATW = new System.Windows.Forms.CheckBox();
             this.buttonBack = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.listViewResult = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // buttonSYM
+            // buttonStart
             // 
-            this.buttonSYM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonSYM.Location = new System.Drawing.Point(42, 161);
-            this.buttonSYM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.buttonSYM.Name = "buttonSYM";
-            this.buttonSYM.Size = new System.Drawing.Size(172, 47);
-            this.buttonSYM.TabIndex = 0;
-            this.buttonSYM.Text = "START SIMULATION";
-            this.buttonSYM.UseVisualStyleBackColor = true;
-            this.buttonSYM.Click += new System.EventHandler(this.buttonSYM_Click);
-            // 
-            // labelResult
-            // 
-            this.labelResult.BackColor = System.Drawing.SystemColors.Info;
-            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelResult.Location = new System.Drawing.Point(122, 265);
-            this.labelResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.labelResult.Name = "labelResult";
-            this.labelResult.Size = new System.Drawing.Size(331, 48);
-            this.labelResult.TabIndex = 2;
-            // 
-            // label5
-            // 
-            this.label5.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(16, 265);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 48);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Result:";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.Location = new System.Drawing.Point(42, 161);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(172, 47);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START SIMULATION";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // label6
             // 
@@ -97,7 +73,7 @@
             // 
             this.checkTemp.AutoSize = true;
             this.checkTemp.Location = new System.Drawing.Point(11, 67);
-            this.checkTemp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkTemp.Margin = new System.Windows.Forms.Padding(2);
             this.checkTemp.Name = "checkTemp";
             this.checkTemp.Size = new System.Drawing.Size(86, 17);
             this.checkTemp.TabIndex = 15;
@@ -108,7 +84,7 @@
             // 
             this.checkCis.AutoSize = true;
             this.checkCis.Location = new System.Drawing.Point(335, 68);
-            this.checkCis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkCis.Margin = new System.Windows.Forms.Padding(2);
             this.checkCis.Name = "checkCis";
             this.checkCis.Size = new System.Drawing.Size(67, 17);
             this.checkCis.TabIndex = 16;
@@ -119,7 +95,7 @@
             // 
             this.checkWilg.AutoSize = true;
             this.checkWilg.Location = new System.Drawing.Point(178, 68);
-            this.checkWilg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkWilg.Margin = new System.Windows.Forms.Padding(2);
             this.checkWilg.Name = "checkWilg";
             this.checkWilg.Size = new System.Drawing.Size(66, 17);
             this.checkWilg.TabIndex = 17;
@@ -130,7 +106,7 @@
             // 
             this.checkNATB.AutoSize = true;
             this.checkNATB.Location = new System.Drawing.Point(11, 127);
-            this.checkNATB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNATB.Margin = new System.Windows.Forms.Padding(2);
             this.checkNATB.Name = "checkNATB";
             this.checkNATB.Size = new System.Drawing.Size(96, 17);
             this.checkNATB.TabIndex = 18;
@@ -141,7 +117,7 @@
             // 
             this.checkNAPB.AutoSize = true;
             this.checkNAPB.Location = new System.Drawing.Point(11, 98);
-            this.checkNAPB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNAPB.Margin = new System.Windows.Forms.Padding(2);
             this.checkNAPB.Name = "checkNAPB";
             this.checkNAPB.Size = new System.Drawing.Size(98, 17);
             this.checkNAPB.TabIndex = 19;
@@ -152,7 +128,7 @@
             // 
             this.checkNATSOL.AutoSize = true;
             this.checkNATSOL.Location = new System.Drawing.Point(178, 127);
-            this.checkNATSOL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNATSOL.Margin = new System.Windows.Forms.Padding(2);
             this.checkNATSOL.Name = "checkNATSOL";
             this.checkNATSOL.Size = new System.Drawing.Size(117, 17);
             this.checkNATSOL.TabIndex = 20;
@@ -163,7 +139,7 @@
             // 
             this.checkNAPSOL.AutoSize = true;
             this.checkNAPSOL.Location = new System.Drawing.Point(178, 98);
-            this.checkNAPSOL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNAPSOL.Margin = new System.Windows.Forms.Padding(2);
             this.checkNAPSOL.Name = "checkNAPSOL";
             this.checkNAPSOL.Size = new System.Drawing.Size(119, 17);
             this.checkNAPSOL.TabIndex = 21;
@@ -174,7 +150,7 @@
             // 
             this.checkNAPW.AutoSize = true;
             this.checkNAPW.Location = new System.Drawing.Point(335, 98);
-            this.checkNAPW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNAPW.Margin = new System.Windows.Forms.Padding(2);
             this.checkNAPW.Name = "checkNAPW";
             this.checkNAPW.Size = new System.Drawing.Size(91, 17);
             this.checkNAPW.TabIndex = 22;
@@ -185,7 +161,7 @@
             // 
             this.checkNATW.AutoSize = true;
             this.checkNATW.Location = new System.Drawing.Point(335, 127);
-            this.checkNATW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkNATW.Margin = new System.Windows.Forms.Padding(2);
             this.checkNATW.Name = "checkNATW";
             this.checkNATW.Size = new System.Drawing.Size(89, 17);
             this.checkNATW.TabIndex = 23;
@@ -196,7 +172,7 @@
             // 
             this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonBack.Location = new System.Drawing.Point(11, 322);
-            this.buttonBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(2);
             this.buttonBack.Name = "buttonBack";
             this.buttonBack.Size = new System.Drawing.Size(442, 37);
             this.buttonBack.TabIndex = 24;
@@ -204,23 +180,35 @@
             this.buttonBack.UseVisualStyleBackColor = true;
             this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
             // 
-            // button1
+            // buttonStop
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(231, 161);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 47);
-            this.button1.TabIndex = 25;
-            this.button1.Text = "STOP SIMULATION";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStop.Location = new System.Drawing.Point(231, 161);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(172, 47);
+            this.buttonStop.TabIndex = 25;
+            this.buttonStop.Text = "STOP SIMULATION";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // listViewResult
+            // 
+            this.listViewResult.HideSelection = false;
+            this.listViewResult.Location = new System.Drawing.Point(12, 220);
+            this.listViewResult.Name = "listViewResult";
+            this.listViewResult.Size = new System.Drawing.Size(442, 97);
+            this.listViewResult.TabIndex = 26;
+            this.listViewResult.UseCompatibleStateImageBehavior = false;
+            this.listViewResult.View = System.Windows.Forms.View.Details;
             // 
             // Simulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(470, 368);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listViewResult);
+            this.Controls.Add(this.buttonStop);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.checkNATW);
             this.Controls.Add(this.checkNAPW);
@@ -232,9 +220,7 @@
             this.Controls.Add(this.checkCis);
             this.Controls.Add(this.checkTemp);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.labelResult);
-            this.Controls.Add(this.buttonSYM);
+            this.Controls.Add(this.buttonStart);
             this.Name = "Simulator";
             this.Text = "Simulator2000";
             this.ResumeLayout(false);
@@ -244,9 +230,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonSYM;
-        private System.Windows.Forms.Label labelResult;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.CheckBox checkTemp;
         private System.Windows.Forms.CheckBox checkCis;
@@ -258,7 +242,8 @@
         private System.Windows.Forms.CheckBox checkNAPW;
         private System.Windows.Forms.CheckBox checkNATW;
         private System.Windows.Forms.Button buttonBack;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.ListView listViewResult;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,66 +12,230 @@ namespace Simulator
 {
     public partial class Simulator : Form
     {
+        Timer timer = new Timer();
+        int id = 1;
+        double temperature, humidity, pressure, battery_voltage, solar_panel_voltage, node_voltage, battery_current, solar_panel_current, node_current;
+
         public Simulator()
         {
             InitializeComponent();
         }
 
-        private void buttonSYM_Click(object sender, EventArgs e)
+        private void buttonStart_Click(object sender, EventArgs e)
         {
             Random random = new Random();
+            listViewResult.Columns.Clear();
+            listViewResult.Items.Clear();
+            listViewResult.Columns.Add("ID");
+            ListViewItem item = new ListViewItem(id.ToString());
 
-            if(checkTemp.Checked == true)
+            buttonStart.Enabled = false;
+            foreach (Control c in Controls)
             {
-                labelResult.Text = random.Next(-20, 70).ToString() + "°C";
+                if(c is CheckBox)
+                {
+                    c.Enabled = false;
+                }
             }
 
-            if(checkWilg.Checked == true)
+            if (checkTemp.Checked)
             {
-                labelResult.Text = random.Next(0, 100).ToString() + "%";
+                listViewResult.Columns.Add("Temperature", 100);
+                temperature = Math.Round(randomDouble(-30, 50), 3);
+                item.SubItems.Add(temperature.ToString() + " °C");
             }
 
-            if(checkCis.Checked == true)
+            if(checkWilg.Checked)
             {
-                labelResult.Text = random.Next(0, 100000).ToString() + "Pa";
+                listViewResult.Columns.Add("Humidity", 100);
+                humidity = Math.Round(randomDouble(0, 100));
+                item.SubItems.Add(humidity.ToString() + "%");
             }
 
-            if(checkNAPB.Checked == true)
+            if(checkCis.Checked)
             {
-                labelResult.Text = random.Next(0, 10).ToString() + "V";
+                listViewResult.Columns.Add("Pressure", 100);
+                pressure = Math.Round(randomDouble(980, 1020));
+                item.SubItems.Add(pressure.ToString() + " hPa");
             }
 
-            if (checkNAPSOL.Checked == true)
+            if(checkNAPB.Checked)
             {
-                labelResult.Text = random.Next(0, 100).ToString() + "V";
+                listViewResult.Columns.Add("Battery Voltage", 100);
+                battery_voltage = Math.Round(randomDouble(2.5, 4.8));
+                item.SubItems.Add(battery_voltage.ToString() + " V");
             }
 
-            if (checkNAPW.Checked == true)
+            if (checkNAPSOL.Checked)
             {
-                labelResult.Text = random.Next(0, 50).ToString() + "V";
+                listViewResult.Columns.Add("Solar Panel Voltage", 100);
+                solar_panel_voltage = Math.Round(randomDouble(28, 32));
+                item.SubItems.Add(solar_panel_voltage.ToString() + " V");
             }
 
-            if (checkNATB.Checked == true)
+            if (checkNAPW.Checked)
             {
-                labelResult.Text = random.Next(0, 5).ToString() + "I";
+                listViewResult.Columns.Add("Node Voltage", 100);
+                node_voltage = Math.Round(randomDouble(28, 32));
+                item.SubItems.Add(node_voltage.ToString() + " V");
             }
 
-            if (checkNATSOL.Checked == true)
+            if (checkNATB.Checked)
             {
-                labelResult.Text = random.Next(0, 30).ToString() + "I";
+                listViewResult.Columns.Add("Battery Current", 100);
+                battery_current = Math.Round(randomDouble(6.0, 9.0));
+                item.SubItems.Add(battery_current.ToString() + " A");
             }
 
-            if (checkNATW.Checked == true)
+            if (checkNATSOL.Checked)
             {
-                labelResult.Text = random.Next(0, 15).ToString() + "I";
+                listViewResult.Columns.Add("Solar Panel Current", 100);
+                solar_panel_current = Math.Round(randomDouble(6, 9));
+                item.SubItems.Add(solar_panel_current.ToString() + " A");
             }
+
+            if (checkNATW.Checked)
+            {
+                listViewResult.Columns.Add("Node Current", 100);
+                node_current = Math.Round(randomDouble(6, 9));
+                item.SubItems.Add(node_current.ToString() + " A");
+            }
+            
+            listViewResult.Items.Insert(0, item);
+            timer.Interval = 5000;
+            timer.Tick += new EventHandler(this.t_Tick);
+            timer.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            id = 1;
+            foreach (Control c in Controls)
+            {
+                if (c is CheckBox)
+                    c.Enabled = true;
+            }
+            buttonStart.Enabled = true;
         }
 
         private void buttonBack_Click(object sender, EventArgs e)
         {
             Menu openMenu = new Menu();
             openMenu.Show();
-            this.Close();
+            Close();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            id++;
+            ListViewItem item = new ListViewItem(id.ToString());
+
+            if (checkTemp.Checked)
+            {
+                double temporary = temperature + Math.Round(randomDouble(-1, 1), 3);
+                while (temporary < -30 || temporary > 50)
+                {
+                    temporary = temperature + Math.Round(randomDouble(-1, 1), 3);
+                }
+                temperature = temporary;
+                item.SubItems.Add(temperature.ToString() + " °C");
+            }
+
+            if (checkWilg.Checked)
+            {
+                double temporary = humidity + Math.Round(randomDouble(-1, 1), 3);
+                while (temporary < 0 || temporary > 100)
+                {
+                    temporary = humidity + Math.Round(randomDouble(-1, 1), 3);
+                }
+                humidity = temporary;
+                item.SubItems.Add(humidity.ToString() + "%");
+            }
+
+            if (checkCis.Checked)
+            {
+                double temporary = pressure + Math.Round(randomDouble(-1, 1), 3);
+                while (temporary < 980 || temporary > 1020)
+                {
+                    temporary = pressure + Math.Round(randomDouble(-1, 1), 3);
+                }
+                pressure = temporary;
+                item.SubItems.Add(pressure.ToString() + " hPa");
+            }
+
+            if (checkNAPB.Checked)
+            {
+                double temporary = battery_voltage + Math.Round(randomDouble(-0.1, 0.1), 3);
+                while (temporary < 2.5 || temporary > 4.8)
+                {
+                    temporary = battery_voltage + Math.Round(randomDouble(-0.1, 0.1), 3);
+                }
+                battery_voltage = temporary;
+                item.SubItems.Add(battery_voltage.ToString() + " V");
+            }
+
+            if (checkNAPSOL.Checked)
+            {
+                double temporary = solar_panel_voltage + Math.Round(randomDouble(-0.5, 0.5), 3);
+                while (temporary < 28 || temporary > 32)
+                {
+                    temporary = solar_panel_voltage + Math.Round(randomDouble(-0.5, 0.5), 3);
+                }
+                solar_panel_voltage = temporary;
+                item.SubItems.Add(solar_panel_voltage.ToString() + " V");
+            }
+
+            if (checkNAPW.Checked)
+            {
+                double temporary = node_voltage + Math.Round(randomDouble(-0.5, 0.5), 3);
+                while (temporary < 28 || temporary > 32)
+                {
+                    temporary = node_voltage + Math.Round(randomDouble(-0.5, 0.5), 3);
+                }
+                node_voltage = temporary;
+                item.SubItems.Add(node_voltage.ToString() + " V");
+            }
+
+            if (checkNATB.Checked)
+            {
+                double temporary = battery_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                while (temporary < 6 || temporary > 9)
+                {
+                    temporary = battery_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                }
+                battery_current = temporary;
+                item.SubItems.Add(battery_current.ToString() + " A");
+            }
+
+            if (checkNATSOL.Checked)
+            {
+                double temporary = solar_panel_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                while (temporary < 6 || temporary > 9)
+                {
+                    temporary = solar_panel_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                }
+                solar_panel_current = temporary;
+                item.SubItems.Add(solar_panel_current.ToString() + " A");
+            }
+
+            if (checkNATW.Checked)
+            {
+                double temporary = node_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                while (temporary < 6 || temporary > 9)
+                {
+                    temporary = node_current + Math.Round(randomDouble(-0.1, 0.1), 3);
+                }
+                node_current = temporary;
+                item.SubItems.Add(node_current.ToString() + " A");
+            }
+            listViewResult.Items.Insert(0, item);
+        }
+
+        private double randomDouble(double min, double max)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (max - min) + min;
         }
     }
 }
