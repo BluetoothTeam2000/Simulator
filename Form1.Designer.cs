8f640df723d0119be0dd0b0feeaa9654e6551d01
KobertRubica <stilic97@gmail.com>
2020-04-24 16:51:27 +0200

    simulator works now almost

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,6 @@
         {
             this.buttonSYM = new System.Windows.Forms.Button();
             this.labelResult = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.checkTemp = new System.Windows.Forms.CheckBox();
@@ -46,16 +42,18 @@
             this.checkNAPW = new System.Windows.Forms.CheckBox();
             this.checkNATW = new System.Windows.Forms.CheckBox();
             this.buttonBack = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonSYM
             // 
             this.buttonSYM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonSYM.Location = new System.Drawing.Point(225, 265);
+            this.buttonSYM.Location = new System.Drawing.Point(42, 161);
+            this.buttonSYM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSYM.Name = "buttonSYM";
-            this.buttonSYM.Size = new System.Drawing.Size(380, 45);
+            this.buttonSYM.Size = new System.Drawing.Size(172, 47);
             this.buttonSYM.TabIndex = 0;
-            this.buttonSYM.Text = "SYMULATE";
+            this.buttonSYM.Text = "START SIMULATION";
             this.buttonSYM.UseVisualStyleBackColor = true;
             this.buttonSYM.Click += new System.EventHandler(this.buttonSYM_Click);
             // 
@@ -63,63 +61,21 @@
             // 
             this.labelResult.BackColor = System.Drawing.SystemColors.Info;
             this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelResult.Location = new System.Drawing.Point(365, 331);
+            this.labelResult.Location = new System.Drawing.Point(122, 265);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelResult.Name = "labelResult";
-            this.labelResult.Size = new System.Drawing.Size(240, 45);
+            this.labelResult.Size = new System.Drawing.Size(331, 48);
             this.labelResult.TabIndex = 2;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(15, 83);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(172, 292);
-            this.listBox1.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.SystemColors.Info;
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(520, 195);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 57);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "1s";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(225, 195);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(272, 57);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Interval for generating new values:";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(15, 12);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(172, 57);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "History";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
             this.label5.BackColor = System.Drawing.SystemColors.HighlightText;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(225, 331);
+            this.label5.Location = new System.Drawing.Point(16, 265);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(117, 45);
+            this.label5.Size = new System.Drawing.Size(93, 48);
             this.label5.TabIndex = 13;
             this.label5.Text = "Result:";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -129,9 +85,10 @@
             this.label6.BackColor = System.Drawing.SystemColors.HighlightText;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(225, 12);
+            this.label6.Location = new System.Drawing.Point(11, 10);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(379, 57);
+            this.label6.Size = new System.Drawing.Size(443, 47);
             this.label6.TabIndex = 14;
             this.label6.Text = "Variable selection";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,109 +96,131 @@
             // checkTemp
             // 
             this.checkTemp.AutoSize = true;
-            this.checkTemp.Location = new System.Drawing.Point(225, 83);
+            this.checkTemp.Location = new System.Drawing.Point(11, 67);
+            this.checkTemp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkTemp.Name = "checkTemp";
-            this.checkTemp.Size = new System.Drawing.Size(112, 21);
+            this.checkTemp.Size = new System.Drawing.Size(86, 17);
             this.checkTemp.TabIndex = 15;
-            this.checkTemp.Text = "Temperatura";
+            this.checkTemp.Text = "Temperature";
             this.checkTemp.UseVisualStyleBackColor = true;
             // 
             // checkCis
             // 
             this.checkCis.AutoSize = true;
-            this.checkCis.Location = new System.Drawing.Point(477, 84);
+            this.checkCis.Location = new System.Drawing.Point(335, 68);
+            this.checkCis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkCis.Name = "checkCis";
-            this.checkCis.Size = new System.Drawing.Size(87, 21);
+            this.checkCis.Size = new System.Drawing.Size(67, 17);
             this.checkCis.TabIndex = 16;
-            this.checkCis.Text = "Ciśnienie";
+            this.checkCis.Text = "Pressure";
             this.checkCis.UseVisualStyleBackColor = true;
             // 
             // checkWilg
             // 
             this.checkWilg.AutoSize = true;
-            this.checkWilg.Location = new System.Drawing.Point(347, 84);
+            this.checkWilg.Location = new System.Drawing.Point(178, 68);
+            this.checkWilg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkWilg.Name = "checkWilg";
-            this.checkWilg.Size = new System.Drawing.Size(99, 21);
+            this.checkWilg.Size = new System.Drawing.Size(66, 17);
             this.checkWilg.TabIndex = 17;
-            this.checkWilg.Text = "Wilgotność";
+            this.checkWilg.Text = "Humidity";
             this.checkWilg.UseVisualStyleBackColor = true;
             // 
             // checkNATB
             // 
             this.checkNATB.AutoSize = true;
-            this.checkNATB.Location = new System.Drawing.Point(225, 156);
+            this.checkNATB.Location = new System.Drawing.Point(11, 127);
+            this.checkNATB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNATB.Name = "checkNATB";
-            this.checkNATB.Size = new System.Drawing.Size(125, 21);
+            this.checkNATB.Size = new System.Drawing.Size(96, 17);
             this.checkNATB.TabIndex = 18;
-            this.checkNATB.Text = "Natężenie BAT";
+            this.checkNATB.Text = "Battery Current";
             this.checkNATB.UseVisualStyleBackColor = true;
             // 
             // checkNAPB
             // 
             this.checkNAPB.AutoSize = true;
-            this.checkNAPB.Location = new System.Drawing.Point(225, 120);
+            this.checkNAPB.Location = new System.Drawing.Point(11, 98);
+            this.checkNAPB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNAPB.Name = "checkNAPB";
-            this.checkNAPB.Size = new System.Drawing.Size(116, 21);
+            this.checkNAPB.Size = new System.Drawing.Size(98, 17);
             this.checkNAPB.TabIndex = 19;
-            this.checkNAPB.Text = "Napięcie BAT";
+            this.checkNAPB.Text = "Battery Voltage";
             this.checkNAPB.UseVisualStyleBackColor = true;
             // 
             // checkNATSOL
             // 
             this.checkNATSOL.AutoSize = true;
-            this.checkNATSOL.Location = new System.Drawing.Point(347, 156);
+            this.checkNATSOL.Location = new System.Drawing.Point(178, 127);
+            this.checkNATSOL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNATSOL.Name = "checkNATSOL";
-            this.checkNATSOL.Size = new System.Drawing.Size(126, 21);
+            this.checkNATSOL.Size = new System.Drawing.Size(117, 17);
             this.checkNATSOL.TabIndex = 20;
-            this.checkNATSOL.Text = "Natężenie SOL";
+            this.checkNATSOL.Text = "Solar Panel Current";
             this.checkNATSOL.UseVisualStyleBackColor = true;
             // 
             // checkNAPSOL
             // 
             this.checkNAPSOL.AutoSize = true;
-            this.checkNAPSOL.Location = new System.Drawing.Point(347, 120);
+            this.checkNAPSOL.Location = new System.Drawing.Point(178, 98);
+            this.checkNAPSOL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNAPSOL.Name = "checkNAPSOL";
-            this.checkNAPSOL.Size = new System.Drawing.Size(117, 21);
+            this.checkNAPSOL.Size = new System.Drawing.Size(119, 17);
             this.checkNAPSOL.TabIndex = 21;
-            this.checkNAPSOL.Text = "Napięcie SOL";
+            this.checkNAPSOL.Text = "Solar Panel Voltage";
             this.checkNAPSOL.UseVisualStyleBackColor = true;
             // 
             // checkNAPW
             // 
             this.checkNAPW.AutoSize = true;
-            this.checkNAPW.Location = new System.Drawing.Point(477, 120);
+            this.checkNAPW.Location = new System.Drawing.Point(335, 98);
+            this.checkNAPW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNAPW.Name = "checkNAPW";
-            this.checkNAPW.Size = new System.Drawing.Size(128, 21);
+            this.checkNAPW.Size = new System.Drawing.Size(91, 17);
             this.checkNAPW.TabIndex = 22;
-            this.checkNAPW.Text = "Napięcie Węzła";
+            this.checkNAPW.Text = "Node Voltage";
             this.checkNAPW.UseVisualStyleBackColor = true;
             // 
             // checkNATW
             // 
             this.checkNATW.AutoSize = true;
-            this.checkNATW.Location = new System.Drawing.Point(477, 156);
+            this.checkNATW.Location = new System.Drawing.Point(335, 127);
+            this.checkNATW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkNATW.Name = "checkNATW";
-            this.checkNATW.Size = new System.Drawing.Size(137, 21);
+            this.checkNATW.Size = new System.Drawing.Size(89, 17);
             this.checkNATW.TabIndex = 23;
-            this.checkNATW.Text = "Natężenie Węzła";
+            this.checkNATW.Text = "Node Current";
             this.checkNATW.UseVisualStyleBackColor = true;
             // 
             // buttonBack
             // 
             this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonBack.Location = new System.Drawing.Point(15, 396);
+            this.buttonBack.Location = new System.Drawing.Point(11, 322);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(590, 45);
+            this.buttonBack.Size = new System.Drawing.Size(442, 37);
             this.buttonBack.TabIndex = 24;
             this.buttonBack.Text = "BACK";
             this.buttonBack.UseVisualStyleBackColor = true;
             this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(231, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 47);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "STOP SIMULATION";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Simulator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 453);
+            this.ClientSize = new System.Drawing.Size(470, 368);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.checkNATW);
             this.Controls.Add(this.checkNAPW);
@@ -254,15 +233,10 @@
             this.Controls.Add(this.checkTemp);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.labelResult);
             this.Controls.Add(this.buttonSYM);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Simulator";
-            this.Text = "Simulator";
+            this.Text = "Simulator2000";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -272,10 +246,6 @@
 
         private System.Windows.Forms.Button buttonSYM;
         private System.Windows.Forms.Label labelResult;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.CheckBox checkTemp;
@@ -288,6 +258,7 @@
         private System.Windows.Forms.CheckBox checkNAPW;
         private System.Windows.Forms.CheckBox checkNATW;
         private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button button1;
     }
 }
 
